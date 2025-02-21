3b39ca18707d2c1d6f39d02b7d267979f3944136
BIGGPIU <114438660+BIGGPIU@users.noreply.github.com>
2025-02-21 13:12:26 -0500

    q11 - you no longer have to manifest your code into working

--- Diyas DSA/q11 - Make Linked List Circular - Easy.py
+++ Diyas DSA/q11 - Make Linked List Circular - Easy.py
@@ -5,6 +5,8 @@ or to put it in simple terms make the first item in the linked list go to the la
 
 this problems basically the same no matter what you do so theres only one test case
 
+You can return any node of your answer once you are finished
+
 """
 
 class TreeNode:
@@ -13,7 +15,6 @@ class TreeNode:
         self.next = next
 
 def circular(head:TreeNode) -> list[int]:
-    # I dont feel like making this work just manifest your code into working
     pass
 
 
@@ -386,8 +387,15 @@ def circular(head:TreeNode) -> list[int]:
 
 
 
-
-
+''' my answer
+first_head = head
+    while head.next.value != None:
+        print(head.next)
+        print(head.value)
+        head = head.next
+    head.next = first_head
+    return first_head
+'''
 
 
 def CreateLLfromTree(List):
@@ -403,19 +411,20 @@ def CreateLLfromTree(List):
     return head
 
 def myanswer(head:TreeNode):
-    shead = head
-    while head.next.value != None:
+    visited = []
+    while head.next != None:
+        visited.append(head)
         head = head.next
-    head.next = shead
-    return shead
+        if head in visited:
+            return True
+    return False
 
 def main() -> str:
     qs = [
-        CreateLLfromTree([1,2,3,4,5]),
         CreateLLfromTree([1,2,3,4,5])
     ]
-
-    if myanswer(qs[0]) != circular(qs[1]):
+    x = circular(qs[0])
+    if not myanswer(x):
         return f'''
 Failed testcase {1}
         '''
